6e158c7d25c1239c89355faa7ac347b70990e513
Jason Teter <jason.iasthai@gmail.com>
2019-11-26 19:41:08 -0600

    Adding message to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+            Jason Teter Says:
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
